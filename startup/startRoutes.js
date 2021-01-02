461bea26316037f5f67435649e5fb1e34645df75
Linna <linnali577@gmail.com>
2021-01-02 16:57:09 -0500

    reordered routes

--- startup/startRoutes.js
+++ startup/startRoutes.js
@@ -7,9 +7,9 @@ const errorHandle = require("../middleware/errorHandle");
 
 module.exports = function (app) {
   app.use(express.json());
-  app.use("/api/tasks", tasks);
-  app.use("/api/users", users);
   app.use("/api/auth", auth);
+  app.use("/api/users", users);
+  app.use("/api/tasks", tasks);
   app.use("/api/goals", goals);
   app.use(errorHandle);
 };
